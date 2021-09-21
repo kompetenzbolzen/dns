51da994a563fd4746410f3b093fa7acb4fde3a20
Jonas Gunz <himself@jonasgunz.de>
2021-09-21 16:38:07 +0200

    Fix tests/record.c

--- tests/record.c
+++ tests/record.c
@@ -22,9 +22,9 @@ START_TEST (test_records_handler) {
 	void* rdata = NULL;
 	ssize_t len;
 
-	len = record_rdata_from_str(&rdata, "69.69.69.68", record_class_from_str("A"));
+	len = record_rdata_from_str(&rdata, "69.69.69.68", record_type_from_str("A"));
 	ck_assert_int_eq(len,4);
-	ck_assert_uint_eq(*((uint32_t*)rdata),0x45454544);
+	ck_assert_uint_eq(*((uint32_t*)rdata),0x44454545);
 
 	free(rdata);
 }
