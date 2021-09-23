9ae50e0efb9f455434f92fe81ca981a79f23856a
Jonas Gunz <himself@jonasgunz.de>
2021-09-23 20:29:43 +0200

    scripts: netfuzz change port

--- scripts/netfuzz.sh
+++ scripts/netfuzz.sh
@@ -2,5 +2,5 @@
 
 while true
 do
-	dd if=/dev/urandom bs=$(echo $RANDOM/100 | bc) count=1 2>/dev/null | nc -cu localhost 53 > /dev/null
+	dd if=/dev/urandom bs=$(echo $RANDOM/100 | bc) count=1 2>/dev/null | nc -cu localhost 5333 > /dev/null
 done
